1b039794fd7ab1d17c2e4c5a190dcd26ca6ab0b6
Charlotte Soneson <charlottesoneson@gmail.com>
2022-01-13 19:43:55 +0100

    Specify versions for all dependencies in conda environment

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,5 +1,9 @@
 #' @importFrom basilisk BasiliskEnvironment
 geosketchenv <- BasiliskEnvironment(
     envname = "env", pkgname = "geosketchR",
-    packages = character(0), pip = c("geosketch==1.2"))
+    packages = c("joblib==1.1.0", "numpy==1.21.5",
+                 "scikit-learn==1.0.2", "scipy==1.7.3", "threadpoolctl==3.0.0"),
+    channels = c("conda-forge"),
+    pip = c("fbpca==1.0", "geosketch==1.2")
+)
 
