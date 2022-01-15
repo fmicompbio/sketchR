88c15f729b4cf09b4c6b24427a430a1b63bab3b3
mbstadler <stadler.michael@gmail.com>
2022-01-15 18:37:12 +0100

    improve test coverage

--- tests/testthat/tests-geosketch.R
+++ tests/testthat/tests-geosketch.R
@@ -6,6 +6,7 @@ n <- 10
 ## ------------------------------------------------------------------------- ##
 ## Miss-specified arguments
 ## ------------------------------------------------------------------------- ##
+## geosketch
 expect_error(geosketch(mat = "error"), "of class 'matrix'")
 expect_error(geosketch(mat = xd), '"N" is missing')
 expect_error(geosketch(N = n), '"mat" is missing')
@@ -25,6 +26,8 @@ id1 <- geosketch(mat = xd, N = n, seed = 42)
 id2 <- geosketch(mat = xd, N = n, seed = 44)
 id3 <- geosketch(mat = xd, N = 2 * n, seed = 42)
 id4 <- geosketch(mat = xd, N = 2 * n, seed = 44)
+id5 <- geosketch(mat = xd, N = n, k = n, seed = 99)
+id6 <- geosketch(mat = xd, N = n, k = n, seed = 99)
 is1 <- geosketch(mat = xs, N = n, seed = 42)
 is2 <- geosketch(mat = xs, N = n, seed = 44)
 
@@ -32,6 +35,8 @@ expect_type(id1, "double")
 expect_type(id2, "double")
 expect_type(id3, "double")
 expect_type(id4, "double")
+expect_type(id5, "double")
+expect_type(id6, "double")
 expect_type(is1, "double")
 expect_type(is2, "double")
 
@@ -39,11 +44,14 @@ expect_length(id1, n)
 expect_length(id2, n)
 expect_length(id3, 2 * n)
 expect_length(id4, 2 * n)
+expect_length(id5, n)
+expect_length(id6, n)
 expect_length(is1, n)
 expect_length(is2, n)
 
 expect_identical(id1, is1)
 expect_identical(id2, is2)
+expect_identical(id5, id6)
 
 expect_identical(id1, c(49, 253, 353, 364, 486, 615, 651, 729, 737, 754))
 expect_identical(id2, c(49, 96, 295, 433, 495, 591, 615, 651, 673, 845))
@@ -51,4 +59,13 @@ expect_identical(id3, c(39, 147, 220, 248, 253, 312, 466, 492, 503, 578, 593,
                         702, 714, 729, 749, 837, 858, 868, 920, 979))
 expect_identical(id4, c(39, 63, 120, 147, 289, 312, 345, 492, 615, 642, 694,
                         714, 737, 754, 832, 841, 920, 933, 936, 979))
+expect_identical(id5, c(37, 49, 145, 253, 274, 615, 651, 789, 852, 966))
 
+
+## ------------------------------------------------------------------------- ##
+## Checks, getGeosketchNames
+## ------------------------------------------------------------------------- ##
+nms <- getGeosketchNames()
+
+expect_type(nms, "character")
+expect_true("gs" %in% nms)
