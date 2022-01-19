11cebacbbee85aa55b8812b90d64ac3eb0e1de71
Charlotte Soneson <charlottesoneson@gmail.com>
2022-01-19 20:06:51 +0100

    Add scsampler

--- R/basilisk.R
+++ R/basilisk.R
@@ -1,9 +1,16 @@
 #' @importFrom basilisk BasiliskEnvironment
 geosketchenv <- BasiliskEnvironment(
-    envname = "env", pkgname = "geosketchR",
+    envname = "geosketch", pkgname = "sketchR",
     packages = c("joblib==1.1.0", "numpy==1.21.5",
                  "scikit-learn==1.0.2", "scipy==1.7.3", "threadpoolctl==3.0.0"),
     channels = c("conda-forge"),
     pip = c("fbpca==1.0", "geosketch==1.2")
+)
+
+scsamplerenv <- BasiliskEnvironment(
+    envname = "scsampler", pkgname = "sketchR",
+    packages = c("numpy==1.21.5", "pandas==1.3.5", "scanpy==1.7.2"),
+    channels = c("bioconda", "conda-forge"),
+    pip = c("scsampler==1.0.0")
 )
 
---
+++ R/scsampler.R
@@ -0,0 +1,90 @@
+#' Get names of scSampler functions
+#'
+#' @return A list of names of objects exposed in the scSampler module
+#' @author Charlotte Soneson
+#'
+#' @examples
+#' getScSamplerNames()
+#'
+#' @export
+#'
+#' @importFrom reticulate import
+#' @importFrom basilisk basiliskStart basiliskRun basiliskStop
+getScSamplerNames <- function() {
+    cl <- basiliskStart(scsamplerenv)
+    scsampler.names <- basiliskRun(cl, function() {
+        X <- reticulate::import("scsampler")
+        names(X)
+    })
+    basiliskStop(cl)
+    scsampler.names
+}
+
+#' Run scSampler to subsample a matrix
+#'
+#' Perform subsampling with the \code{scSampler} python package.
+#'
+#' The first time this function is run, it will create a conda environment
+#' containing the \code{scSampler} package.
+#' This is done via the \code{basilisk} R/Bioconductor package - see the
+#' documentation for that package for troubleshooting.
+#'
+#' @param mat m x n matrix. Samples (the dimension along which to subsample)
+#'     should be in the rows, features in the columns.
+#' @param N Numeric scalar, the number of samples to retain.
+#' @param random_split Numeric scalar, the number of parts to randomly
+#'     split the data into before subsampling within each part. A larger
+#'     value will speed up computations, but give less optimal results.
+#' @param seed Numeric scalar, passed to \code{scsampler} to seed the random
+#'     number generator.
+#'
+#' @examples
+#' x <- matrix(rnorm(500), nrow = 100)
+#' scsampler(mat = x, N = 10)
+#'
+#' @references
+#' Song et al (2022): scSampler: fast diversity-preserving subsampling of
+#' large-scale single-cell transcriptomic data.
+#' bioRxiv doi:10.1101/2022.01.15.476407
+#'
+#' @author Charlotte Soneson, Michael Stadler
+#'
+#' @return A numeric vector with indices to retain.
+#'
+#' @export
+#'
+#' @importFrom DelayedArray is_sparse
+scsampler <- function(mat, N, random_split = 1, seed = 0) {
+    ## --------------------------------------------------------------------- ##
+    ## Check input arguments
+    ## --------------------------------------------------------------------- ##
+    if (DelayedArray::is_sparse(mat)) {
+        mat <- as.matrix(mat)
+    }
+    .assertVector(x = mat, type = "matrix")
+    .assertScalar(x = N, type = "numeric",
+                  rngIncl = c(1, nrow(mat)))
+    N <- as.integer(N)
+    .assertScalar(x = random_split, type = "numeric",
+                  rngIncl = c(1, nrow(mat)))
+    random_split <- as.integer(random_split)
+    .assertScalar(x = seed, type = "numeric")
+    seed <- as.integer(seed)
+
+    ## --------------------------------------------------------------------- ##
+    ## Run scSampler
+    ## --------------------------------------------------------------------- ##
+    idx <- basiliskRun(env = scsamplerenv, fun = .run_scsampler,
+                       mat = mat, n_obs = N, random_state = seed,
+                       random_split = random_split)
+    idx
+}
+
+# Internal function to run scSampler
+.run_scsampler <- function(mat, n_obs, random_state, random_split) {
+    scs <- reticulate::import("scsampler")
+    sketch_index <- scs$scsampler(mat, n_obs = n_obs,
+                                  random_state = random_state,
+                                  random_split = random_split)
+    as.numeric(unlist(sketch_index[[2]]) + 1)
+}
