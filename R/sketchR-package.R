5498bdaceafe78ce8420fa56b2f094890637d067
Charlotte Soneson <charlottesoneson@gmail.com>
2024-02-22 20:35:28 +0100

    Add package man page

---
+++ R/sketchR-package.R
@@ -0,0 +1,14 @@
+#' sketchR - an R interface for python subsampling/sketching algorithms
+#'
+#' The sketchR package provides an R interface for various subsampling
+#' algorithms implemented in python packages. Currently, interfaces to the
+#' geosketch and scSampler python packages are implemented, in the functions
+#' geosketch() and scsampler(), respectively. In addition the package also
+#' provides diagnostic plots to evaluate the subsampling.
+#' More details on how to get started and incorporate the subsampling into
+#' an scRNA-seq workflow are provided in the vignette.
+#'
+#' @author Charlotte Soneson
+#' @author Michael Stadler
+#' @keywords internal
+"_PACKAGE"
